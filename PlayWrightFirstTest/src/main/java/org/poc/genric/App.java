06e7c371cdec94c0d6b04417b86f19ac10449496
muditt1994 <muditt1994@gmail.com>
2024-07-31 23:33:22 +0530

    My initial upload

---
+++ PlayWrightFirstTest/src/main/java/org/poc/genric/App.java
@@ -0,0 +1,25 @@
+package org.poc.genric;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+import java.nio.file.Paths;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
+            Page page = browser.newPage();
+            page.navigate("https://playwright.dev/");
+            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
+        }
+    }
+}
